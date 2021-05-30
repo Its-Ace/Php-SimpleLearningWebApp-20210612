9ad72251a6a8f513a627d78515801241dcbf92c7
Its-Ace <yaseen.asif.125@gmail.com>
2021-05-30 07:15:07 -0700

    php Implementation and Front end changes

--- src/home.php
+++ src/home.php
@@ -1,7 +1,8 @@
 <?php
-    $test = 0;
-    include("connection_test.php");
-    test_connection();
+$test = 0;
+include("connection_test.php");
+require_once("./config.php");
+test_connection();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,37 +12,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link type = "img/png" rel = "shortcut icon" href="favicon.ico.png">
+    <script src="script/jquery-3.6.0.min.js"></script>
+    <script src="./script/script.js"></script>
+    <link type="img/png" rel="shortcut icon" href="favicon.ico.png">
     <title>Home</title>
 </head>
 
 <body>
-    <header class = "header-default">
+    <header class="header-default">
         <nav class="nav-main">
             <div class="brand">
-                <img src="./img/default.png" alt="" style="width: 100px; height: 100px; padding-top: 10px">
-            </div>   
+                <img src="./img/default.png" alt="" style="width: 100px; height: 100px; padding-top: 10px;margin-bottom: 11px;">
+            </div>
             <ul class="nav-contained">
                 <li>
-                    <a href = "home.html" class = "active-page" >Home</a>
+                    <a href="./home.php" class="active-page">Home</a>
                 </li>
                 <li>
-                    <a href = "Howto.html">How to use?</a>
+                    <a href="./Howto.php">How to use?</a>
                 </li>
             </ul>
             <div class="signin-button">
-                <a href="logon.php">
+                <a href="./logon.php">
                     <span>Sign in</span>
                 </a>
             </div>
         </nav>
-    </header> 
+    </header>
 
     <div class="page-main-container">
+
+        <div class="slideshow-container">
         
-        <div class="body-main-container">
+        <div class="left-main-container">
             <h2>Riphah<br>International University</h2>
-            <img src = "./img/logo.png" alt="logo">
+            <img src="./img/logo.png" alt="logo">
             <div class="container">
                 <p>We are here to solve your problems</p>
                 <ul>
@@ -51,13 +56,13 @@
                     <li>
                         Point 2
                     </li>
-                </ul> 
+                </ul>
             </div>
-               
+
         </div>
 
     </div>
-   
+
 </body>
 
 <footer>
